2054db2874e83437c4177a2756518be6f04cc6d4
Jiya <coderaholic21@gmail.com>
2024-06-01 10:17:47 +0530

    changes

--- bin/react-fire.js
+++ bin/react-fire.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-const path = require('path');
-
 // Import the utility functions
 const { createFirebaseFile } = require('./fireBaseUtils');
 const { createHomeComponent } = require('./Home');
@@ -9,9 +7,34 @@ const { createLoginComponent } = require('./Login');
 const { createSignUpComponent } = require('./SignUp');
 const { createSignOutComponent } = require('./SignOut');
 const { createAppComponent } = require('./App');
+const { exec } = require('child_process');
 
 // Check if the command is 'create-auth'
 if (process.argv[2] === 'create-auth') {
+       // Install react-router-dom
+       exec('npm i react-router-dom', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error installing react-router-dom: ${error}`);
+          return;
+        }
+        console.log(`react-router-dom installed! ${stdout}`);
+        if (stderr) {
+          console.error(`npm stderr: ${stderr}`);
+        }
+      });
+    
+      // Install firebase
+      exec('npm i firebase', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error installing firebase: ${error}`);
+          return;
+        }
+        console.log(`firebase installed! ${stdout}`);
+        if (stderr) {
+          console.error(`npm stderr: ${stderr}`);
+        }
+      });
+
   // Call the createFirebaseFile function
   if (createFirebaseFile()) {
     createAppComponent();
